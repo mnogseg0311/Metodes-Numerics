60829ddb278632401ed5e8f140b18317671df204
mnoguera <martanogseg@gmail.com>
2023-02-14 21:32:30 +0100

    problema 1

---
+++ practica1/problema1/asimple.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int	main()
+{
+	float	x;
+	float	fx;
+
+	x = 1.2E-5;
+	if (x == 0)
+		fx = 1/2;
+	else
+		fx = (1 - cosf(x))/(x*x);
+	printf("%.8G\n", fx);
+	return (0);
+}
