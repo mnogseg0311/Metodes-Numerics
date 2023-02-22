4497a57c172587d2f4d4cbfca8af2b042bcf9d98
Marta Noguera Segura <mnoguera@car12s2.42barcelona.com>
2023-02-22 16:12:11 +0100

    000

--- practica1/problema3/adoube.c
+++ practica1/problema3/adoube.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <math.h>
 
-int	calculate_var_2(float *vect, int n)
+int	calculate_var_2(double *vect, int n)
 {
-	float	sn2;
-	float	x;
+	double	sn2;
+	double	x;
 	
 	x = 0;
 	for (int i = 0; i < n; i++)
@@ -14,13 +14,13 @@ int	calculate_var_2(float *vect, int n)
 	for (int i = 0; i < n; i++)
 		sn2 += vect[i]*vect[i];
 	sn2 = (sn2 - x)/(n - 1);
-	return (sqrtf(sn2));
+	return (sqrt(sn2));
 }
 
-int	calculate_var_1(float *vect, int n)
+int	calculate_var_1(double *vect, int n)
 {
-	float	sn2;
-	float	x;
+	double	sn2;
+	double	x;
 
 	x = 0;
 	for (int i = 0; i < n; i++)
@@ -30,20 +30,20 @@ int	calculate_var_1(float *vect, int n)
 	for (int i = 0; i < n; i++)
 		sn2 += (vect[i] - x)*(vect[i] - x);
 	sn2 = sn2/(n - 1);
-	return (sqrtf(sn2));
+	return (sqrt(sn2));
 }
 
 int	main()
 {
-	float	vect[] = {543/96, 300/6, 2E-2, -50/7, 5/897, 7/8};
-	float	variance1;
-	float	variance2;
+	double	vect[] = {543/96, 300/6, 2E-2, -50/7, 5/897, 7/8};
+	double	variance1;
+	double	variance2;
 	int		len;
 
 	len = 6;
 	variance1 = calculate_var_1(vect, len);
 	variance2 = calculate_var_2(vect, len);
-	printf("first formula:\nvariance=%.8G\n", variance1);
-	printf("second formula:\nvariance=%.8G\n", variance2);
+	printf("first formula:\nvariance=%.16G\n", variance1);
+	printf("second formula:\nvariance=%.16G\n", variance2);
 	return (0);
 }
